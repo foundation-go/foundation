036bad3640c19fae8ced368820a75819f8fcc8ee
Rinat Shaykhutdinov <mail@rinatshay.com>
2023-07-31 04:11:21 +0300

    feat: make `GenerateRandomString` panic instead of returning an error

--- rand.go
+++ rand.go
@@ -11,19 +11,19 @@ const (
 	Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 )
 
-// GenerateRandomString generates a random string of length n.
-func GenerateRandomString(n int) (string, FoundationError) {
+// GenerateRandomString generates a random string of length n. Panics if an error occurs.
+func GenerateRandomString(n int) string {
 	var sb strings.Builder
 	max := big.NewInt(int64(len(Alphabet)))
 
 	for i := 0; i < n; i++ {
 		randomInt, err := rand.Int(rand.Reader, max)
 		if err != nil {
-			return "", NewInternalError(err, "failed to generate random string")
+			panic(err)
 		}
 
 		sb.WriteByte(Alphabet[randomInt.Int64()])
 	}
 
-	return sb.String(), nil
+	return sb.String()
 }
