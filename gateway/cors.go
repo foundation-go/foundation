a8a7c9cd5b17139a2d6bb285428fbae12a56b6d1
Vladislav Latish <slott22@yandex.ru>
2023-10-31 18:56:21 +0200

    feat: add CORS settings for gateway service

--- gateway/cors.go
+++ gateway/cors.go
@@ -9,10 +9,10 @@ import (
 	fhttp "github.com/ri-nat/foundation/http"
 )
 
-// TODO: Make these configurable on the Service level.
 var (
 	corsAllowedOrigin = "*"
 	corsMaxAge        = 24 * time.Hour
+	corsAllowMethods  = []string{http.MethodPost}
 
 	corsAllowedHeaders = []string{
 		fhttp.HeaderAccept,
@@ -32,20 +32,85 @@ var (
 	}
 )
 
-// WithCORSEnabled is a middleware that enables CORS for the given handler.
-func WithCORSEnabled(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set(fhttp.HeaderAccessControlAllowOrigin, corsAllowedOrigin)
-		w.Header().Set(fhttp.HeaderAccessControlAllowMethods, http.MethodPost)
-		w.Header().Set(fhttp.HeaderAccessControlAllowHeaders, strings.Join(corsAllowedHeaders, ", "))
-		w.Header().Set(fhttp.HeaderAccessControlExposeHeaders, strings.Join(corsExposedHeaders, ", "))
-		w.Header().Set(fhttp.HeaderAccessControlMaxAge, fmt.Sprintf("%d", int(corsMaxAge.Seconds())))
+// CORSOptions represents the options for CORS.
+type CORSOptions struct {
+	// AllowedOrigin is the allowed origin for CORS.
+	AllowedOrigin string
+	// MaxAge is the max age for CORS.
+	MaxAge time.Duration
+	// AllowedHeaders are the allowed headers for CORS.
+	AllowedHeaders []string
+	// ExposedHeaders are the exposed headers for CORS.
+	ExposedHeaders []string
+	// AllowedMethods are the allowed methods for CORS.
+	AllowedMethods []string
+}
 
-		// If the request is an OPTIONS request, we can safely return here.
-		if r.Method == http.MethodOptions {
-			return
+func NewCORSOptions() *CORSOptions {
+	return &CORSOptions{}
+}
+
+func getUniqueStrings(input []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+
+	for _, entry := range input {
+		if !keys[entry] {
+			keys[entry] = true
+			list = append(list, entry)
 		}
+	}
 
-		handler.ServeHTTP(w, r)
-	})
+	return list
+}
+
+func setDefaultCORSOptions(options *CORSOptions) *CORSOptions {
+	if options.AllowedOrigin == "" {
+		options.AllowedOrigin = corsAllowedOrigin
+	}
+
+	if options.MaxAge == 0 {
+		options.MaxAge = corsMaxAge
+	}
+
+	options.AllowedHeaders = getUniqueStrings(append(options.AllowedHeaders, corsAllowedHeaders...))
+	options.ExposedHeaders = getUniqueStrings(append(options.ExposedHeaders, corsExposedHeaders...))
+	options.AllowedMethods = getUniqueStrings(append(options.AllowedMethods, corsAllowMethods...))
+
+	return options
+}
+
+// WithCORSEnabled is a middleware that enables CORS for the given handler.
+func WithCORSEnabled(options *CORSOptions) func(http.Handler) http.Handler {
+	options = setDefaultCORSOptions(options)
+
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			origin := r.Header.Get("Origin")
+
+			if origin == "" {
+				http.Error(w, "Missing Origin header", http.StatusBadRequest)
+				return
+			}
+
+			if options.AllowedOrigin != "*" && origin != options.AllowedOrigin {
+				http.Error(w, fmt.Sprintf("CORS origin %s not allowed", origin), http.StatusForbidden)
+				return
+			}
+
+			w.Header().Set(fhttp.HeaderAccessControlAllowOrigin, options.AllowedOrigin)
+			w.Header().Set(fhttp.HeaderAccessControlAllowMethods, strings.Join(options.AllowedMethods, ", "))
+			w.Header().Set(fhttp.HeaderAccessControlAllowHeaders, strings.Join(options.AllowedHeaders, ", "))
+			w.Header().Set(fhttp.HeaderAccessControlExposeHeaders, strings.Join(options.ExposedHeaders, ", "))
+			w.Header().Set(fhttp.HeaderAccessControlMaxAge, fmt.Sprintf("%d", int(options.MaxAge.Seconds())))
+
+			// If the request is an OPTIONS request, we can safely return here.
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
+
+			handler.ServeHTTP(w, r)
+		})
+	}
 }
